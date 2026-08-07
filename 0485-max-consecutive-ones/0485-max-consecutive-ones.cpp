a49541e8b2ef88a671406c6b7d6a7270f0c67245
vivekkumarin <vkumar823522@gmail.com>
2026-08-08 00:45:45 +0530

    Time: 4 ms (9.24%), Space: 50.2 MB (34.73%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -6,7 +6,7 @@ public:
         for(int i=0;i<nums.size();i++){
             if(nums[i]==1){
                 count+=1;
-                maxi=max(count,maxi);
+                maxi=max(maxi,count);
             }else{
                 count=0;
             }
