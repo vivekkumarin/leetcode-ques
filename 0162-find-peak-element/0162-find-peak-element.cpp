bbfa65f83263aef0235bf6499b03e7e54dbe41b1
vivekkumarin <vkumar823522@gmail.com>
2026-08-28 00:15:46 +0530

    Time: 0 ms (100%), Space: 12.8 MB (10.53%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -11,9 +11,8 @@ public:
             if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]) return mid;
             else if(nums[mid]>nums[mid-1]) low=mid+1;
             else if(nums[mid]>nums[mid+1]) high=mid-1;
-            else{
-                high=mid-1;
-            }
+            else high=mid-1;
+            
         }
         return -1;
         
